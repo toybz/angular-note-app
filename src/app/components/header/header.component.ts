fb8db558a926f4463cde76a8d9ed234e07088167
Toyeeb-Abdulrahmon <131885912+Toyeeb-Abdulrahmon@users.noreply.github.com>
2024-05-05 16:51:46 +0100

    update header component implementation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,17 +2,27 @@ import { Component, Input } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatCard, MatCardContent } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
-import { MatMiniFabButton } from '@angular/material/button';
+import { MatIconButton, MatMiniFabButton } from '@angular/material/button';
+import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIcon, MatCard, MatCardContent, RouterLink, MatMiniFabButton],
+  imports: [
+    MatIcon,
+    MatCard,
+    MatCardContent,
+    RouterLink,
+    MatMiniFabButton,
+    MatToolbar,
+    MatIconButton,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   @Input() showBackButton = true;
+  @Input() title = 'Cocus Note';
 
   goBack() {
     history.back();
