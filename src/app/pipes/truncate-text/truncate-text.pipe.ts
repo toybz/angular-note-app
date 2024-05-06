7afee8503f3b613db8906e93248dc854dfb65c87
Toyeeb-Abdulrahmon <131885912+Toyeeb-Abdulrahmon@users.noreply.github.com>
2024-05-06 10:30:03 +0100

    add feat truncate text pipe

---
+++ src/app/pipes/truncate-text/truncate-text.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'truncateText',
+  standalone: true,
+})
+export class TruncateTextPipe implements PipeTransform {
+  transform(value: string, [limit]: [number]): unknown {
+    if (value.length <= limit) {
+      return value;
+    } else {
+      return value.slice(0, limit) + '...';
+    }
+  }
+}
