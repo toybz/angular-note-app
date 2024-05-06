b61b72f402855f391931b966d1c2bdaee7b22bd2
Toyeeb-Abdulrahmon <131885912+Toyeeb-Abdulrahmon@users.noreply.github.com>
2024-05-06 15:12:10 +0100

    test: unit test header component

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,6 +1,6 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {HeaderComponent} from './header.component';
+import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,9 +8,8 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
-    })
-      .compileComponents();
+      imports: [HeaderComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
@@ -19,5 +18,32 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show back button if #showBackbutton is true', () => {
+    component.showBackButton = true;
+    const backButton = fixture.nativeElement.querySelector(
+      'button[data-test-id="back-button"]',
+    );
+    expect(backButton).toBeTruthy();
+  });
+
+  it('should not show back button if #showBackbutton is false', () => {
+    component.showBackButton = false;
+    fixture.detectChanges();
+    const backButton = fixture.nativeElement.querySelector(
+      'button[data-test-id="back-button"]',
+    );
+    expect(backButton).toBeFalsy();
+  });
+
+  it('should show the passed in #title', () => {
+    const testTitle = 'Test Title';
+    component.title = testTitle;
+    fixture.detectChanges();
+    const titleEl = fixture.nativeElement.querySelector(
+      'span[data-test-id="title"]',
+    );
+    expect(titleEl.textContent).toBe(testTitle);
   });
 });
