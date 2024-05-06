b7dadcf4f4df7c179232352321d92a1ad58a08d9
Toyeeb-Abdulrahmon <131885912+Toyeeb-Abdulrahmon@users.noreply.github.com>
2024-05-06 10:29:39 +0100

    update ui improvements

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -7,11 +7,21 @@ import {
   MatCardTitle,
 } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
+import { MatButton } from '@angular/material/button';
+import { TruncateTextPipe } from '../../pipes/truncate-text/truncate-text.pipe';
 
 @Component({
   selector: 'app-notes',
   standalone: true,
-  imports: [MatCard, MatCardContent, MatCardTitle, MatCardHeader, RouterLink],
+  imports: [
+    MatCard,
+    MatCardContent,
+    MatCardTitle,
+    MatCardHeader,
+    RouterLink,
+    MatButton,
+    TruncateTextPipe,
+  ],
   templateUrl: './notes.component.html',
   styleUrl: './notes.component.scss',
 })
