5c9f2abb94e046985eb82b2ac45289989748e05e
Toyeeb-Abdulrahmon <131885912+Toyeeb-Abdulrahmon@users.noreply.github.com>
2024-05-25 17:53:01 +0100

    feat: add crud ops for storage service

--- src/app/services/storage/storage.service.ts
+++ src/app/services/storage/storage.service.ts
@@ -8,6 +8,14 @@ import { LocalStorageKeysT } from '../../models/local-storage.model';
 export class StorageService {
   constructor() {}
 
+  public getDatum(tableName: LocalStorageKeysT, id: string) {
+    const localData = JSON.parse(
+      localStorage.getItem(LocalStorageKeys[tableName]) || '[]',
+    );
+    const datum = localData.find((item: any) => item.id === id);
+    return datum || undefined;
+  }
+
   public getData(tableName: LocalStorageKeysT) {
     const localData = localStorage.getItem(LocalStorageKeys[tableName]);
     if (!localData) {
@@ -21,6 +29,31 @@ export class StorageService {
       localStorage.getItem(LocalStorageKeys[tableName]) || '[]',
     );
     const newData = JSON.stringify([...localData, data]);
+    localStorage.setItem(LocalStorageKeys[tableName], newData);
+  }
+
+  public editData<T extends { id: string }>(
+    tableName: LocalStorageKeysT,
+    data: T,
+  ) {
+    const localData = JSON.parse(
+      localStorage.getItem(LocalStorageKeys[tableName]) || '[]',
+    ) as T[];
+    const itemToEditIndex = localData.findIndex((item) => item.id === data.id);
+    localData[itemToEditIndex] = {
+      ...localData[itemToEditIndex]!,
+      ...data,
+    };
+    const newData = JSON.stringify(localData);
+    localStorage.setItem(LocalStorageKeys[tableName], newData);
+  }
+
+  public deleteData(tableName: LocalStorageKeysT, id: string) {
+    const localData = JSON.parse(
+      localStorage.getItem(LocalStorageKeys[tableName]) || '[]',
+    );
+    const unDeletedData = localData.filter((item: any) => item.id !== id);
+    const newData = JSON.stringify(unDeletedData);
     localStorage.setItem(LocalStorageKeys[tableName], newData);
   }
 }
