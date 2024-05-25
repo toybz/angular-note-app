4bb33ee3236d9a6f39ab6ff5a5308eee5f1fa1e3
Toyeeb-Abdulrahmon <131885912+Toyeeb-Abdulrahmon@users.noreply.github.com>
2024-05-25 17:54:31 +0100

    fix: fix userLoggedin guard

--- src/app/guards/user-logged-in/user-logged-in.guard.ts
+++ src/app/guards/user-logged-in/user-logged-in.guard.ts
@@ -5,7 +5,6 @@ import { AuthService } from '../../services/auth/auth.service';
 export const userLoggedInGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
-  return true;
   if (!authService.user()) {
     router.navigateByUrl('auth/login');
   }
